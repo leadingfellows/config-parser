3c32a985b95c2340c1dd72f387151d8380eba9d2
just_like_good_vibes <just_like_good_vibes@792018.no-reply.drupal.org>
2019-07-23 17:11:27 +0200

    upated behavior in case of yaml parsing

--- src/ConfigParser.php
+++ src/ConfigParser.php
@@ -3,16 +3,16 @@
 namespace leadingfellows\utils;
 
 class ConfigParser {
-	
-	protected $configuration;
-	protected $history;
+    
+    protected $configuration;
+    protected $history;
 
-	function __construct() {
-		$this->configuration = array();
-	}
+    function __construct() {
+        $this->configuration = array();
+    }
 
 
-	public static function env_to_array_custom(array $environment) {
+    public static function env_to_array_custom(array $environment) {
         $result = array();
         foreach ($environment as $flat => $value) {
             $keys = explode('__', $flat);
@@ -45,56 +45,58 @@ class ConfigParser {
         return $result;
     }
 
-	/**
+    /**
      *
      */
-	function addYaml($yml_filepath) {
-		if (!file_exists($yml_filepath)) {
-			return null;
-		}
-		$yml_cfg = (array)\Symfony\Component\Yaml\Yaml::parse(file_get_contents($yml_filepath), true, false, false);
-		$this->history [] = array("path" => $yml_filepath, "type" => "yaml", "value" => $yml_cfg);
-		$this->configuration = array_replace_recursive($this->configuration, $yml_cfg);
-		return $yml_cfg;
-	}
+    function addYaml($yml_filepath_or_string) {
+      $path = null;
+        if (file_exists($yml_filepath_or_string)) {
+      $path = $yml_filepath_or_string;
+      $yml_filepath_or_string = file_get_contents($yml_filepath_or_string);
+        }
+        $yml_cfg = (array)\Symfony\Component\Yaml\Yaml::parse($yml_filepath_or_string, true, false, false);
+        $this->history [] = array("yml" => $yml_filepath_or_string, "path" => $path, "type" => "yaml", "value" => $yml_cfg);
+        $this->configuration = array_replace_recursive($this->configuration, $yml_cfg);
+        return $yml_cfg;
+    }
 
-	/**
+    /**
      *
      */
-	function addDotEnv($dotenv_filepath, $prefix=null) {
-		if (!file_exists($yml_filepath)) {
-			return null;
-		}
-		$loader = new \josegonzalez\Dotenv\Loader([$dotenv_filepath]);
-		//	$loader->setFilters(['josegonzalez\Dotenv\Filter\UnderscoreArrayFilter']);
-		$loader->setFilters([ConfigParser::env_to_array_custom]);
-		$loader->parse();
-		$loader->filter();
-		if($prefix !== null) {
-			$loader->prefix($prefix);
-		}
-		$dotenv_cfg = $loader->toArray();
-		$this->history [] = array("path" => $dotenv_filepath, "type" => "dotenv", "value" => $dotenv_cfg);
-		$this->configuration = array_replace_recursive($this->configuration, $dotenv_cfg);
-		return $dotenv_cfg;
-	}
+    function addDotEnv($dotenv_filepath, $prefix=null) {
+        if (!file_exists($dotenv_filepath)) {
+            return null;
+        }
+        $loader = new \josegonzalez\Dotenv\Loader([$dotenv_filepath]);
+        //    $loader->setFilters(['josegonzalez\Dotenv\Filter\UnderscoreArrayFilter']);
+        $loader->setFilters([ConfigParser::env_to_array_custom]);
+        $loader->parse();
+        $loader->filter();
+        if($prefix !== null) {
+            $loader->prefix($prefix);
+        }
+        $dotenv_cfg = $loader->toArray();
+        $this->history [] = array("path" => $dotenv_filepath, "type" => "dotenv", "value" => $dotenv_cfg);
+        $this->configuration = array_replace_recursive($this->configuration, $dotenv_cfg);
+        return $dotenv_cfg;
+    }
 
 
-	function add($type, $path) {
-		switch($type) {
-			case "yaml":
-				$this->addYaml($path);
-				break;
-			case "dotenv":
-				$this->addDotEnv($path);
-				break;
-			default:
-				throw new \Exception("invalid type '" . $type . "'");
-		}
-	}
+    function add($type, $path) {
+        switch($type) {
+            case "yaml":
+                $this->addYaml($path);
+                break;
+            case "dotenv":
+                $this->addDotEnv($path);
+                break;
+            default:
+                throw new \Exception("invalid type '" . $type . "'");
+        }
+    }
 
-	function toArray() {
-		return (array)$this->configuration;
-	}
+    function toArray() {
+        return (array)$this->configuration;
+    }
 
 }
